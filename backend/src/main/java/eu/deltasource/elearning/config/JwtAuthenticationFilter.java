bc771beb11f777204b7806a4442b06addfb5f71c
michaela03 <mihaelakolarova@deltasource.eu>
2025-07-09 09:57:31 +0300

    Refactor JWT configuration and update Docker Compose for improved application structure

--- backend/src/main/java/eu/deltasource/elearning/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/eu/deltasource/elearning/config/JwtAuthenticationFilter.java
@@ -6,12 +6,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -19,6 +21,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Autowired
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
