7acfaca649183c6dcf559d854aed7d509c4a1284
mihaelakolarova03 <mihaelakolarova@deltasource.eu>
2025-07-11 10:59:47 +0300

    Delete src directory

--- src/components/common/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import { Alert } from "react-bootstrap";
-
-export default function ErrorMessage({ message }: { message: string }) {
-    return (
-        <Alert variant="danger" className="my-3">
-            {message}
-        </Alert>
-    );
-}
--- src/components/common/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "react-bootstrap";
-
-export default function Loader() {
-    return (
-        <div className="text-center my-4">
-            <Spinner animation="border" role="status" />
-        </div>
-    );
-}
