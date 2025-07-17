d1f2eb25ae95b3c5447c5ea32eb1f86ec3bac9b7
michaela03 <mihaelakolarova@deltasource.eu>
2025-07-17 11:05:23 +0300

    Add Spring Boot Actuator and configure management endpoints

--- backend/src/main/java/eu/deltasource/elearning/config/OpenApiConfig.java
+++ backend/src/main/java/eu/deltasource/elearning/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 /**
  * Configuration class for customizing the OpenAPI documentation for the online learning platform.
@@ -28,6 +31,14 @@ public class OpenApiConfig {
                         .title("Online learning platform")
                         .description("Watch videos on different IT related topics")
                         .version("1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .servers(Arrays.asList(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Main API server"),
+                        new Server()
+                                .url("http://localhost:8080/actuator")
+                                .description("Actuator endpoints")
+                ));
     }
 }
