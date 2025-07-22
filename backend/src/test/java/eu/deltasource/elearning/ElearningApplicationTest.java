7b34e28647b32009651ca5606012bd17ba1741a4
michaela03 <mihaelakolarova@deltasource.eu>
2025-07-22 11:18:31 +0300

    Refactor ElearningApplicationTest to extend TestContainerConfig for better test organization

--- backend/src/test/java/eu/deltasource/elearning/ElearningApplicationTest.java
+++ backend/src/test/java/eu/deltasource/elearning/ElearningApplicationTest.java
@@ -2,32 +2,13 @@ package eu.deltasource.elearning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
 
 /**
  * Test class for ElearningApplication.
  * This ensures that the Spring Boot application context loads successfully.
  */
 @SpringBootTest
-class ElearningApplicationTest {
-
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("testdb")
-            .withUsername("user")
-            .withPassword("password");
-
-    static {
-        mysql.start();
-    }
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-    }
+class ElearningApplicationTest extends TestContainerConfig {
 
     @Test
     void contextLoads() {
