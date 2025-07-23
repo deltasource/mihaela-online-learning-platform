63927ef8f38f999b0c85999821bfb27d3bdae455
michaela03 <mihaelakolarova@deltasource.eu>
2025-07-23 14:30:43 +0300

    Implement Liquibase migration tool and add initial database changelog

--- backend/src/test/java/eu/deltasource/elearning/ElearningApplicationTest.java
+++ backend/src/test/java/eu/deltasource/elearning/ElearningApplicationTest.java
@@ -2,13 +2,15 @@ package eu.deltasource.elearning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * Test class for ElearningApplication.
  * This ensures that the Spring Boot application context loads successfully.
  */
 @SpringBootTest
-class ElearningApplicationTest extends TestContainerConfig {
+@ContextConfiguration(classes = TestContainerConfig.class)
+class ElearningApplicationTest {
 
     @Test
     void contextLoads() {
