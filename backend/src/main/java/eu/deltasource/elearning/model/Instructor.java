97ebc8cd641b2379648023ad8108c75c3bc97084
michaela03 <mihaelakolarova@deltasource.eu>
2025-07-25 08:40:11 +0300

    Add database migration and update application properties for user management

--- backend/src/main/java/eu/deltasource/elearning/model/Instructor.java
+++ backend/src/main/java/eu/deltasource/elearning/model/Instructor.java
@@ -25,6 +25,9 @@ public class Instructor {
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(unique = true, nullable = false)
+    private String username;
+
     @Column(nullable = false)
     private String firstName;
 
